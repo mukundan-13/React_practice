29c2dfc90fb2df5141dedef416fbf48f76d5de33
mukundan-13 <mukundanproff@gmail.com>
2026-01-21 11:50:12 +0530

    keys & maps added

--- day1_learning/my-react-app/src/components/Garage.jsx
+++ day1_learning/my-react-app/src/components/Garage.jsx
@@ -3,10 +3,17 @@ import Apple from "./Apple";
 function Garage() {
 
   const isDoorOpened=false;
-  const carInfo={}
-  //const carInfo={brand:"Ferrari",color:"Black"}
+  //const carInfo={}
+  const carInfo={brand:"Ferrari",color:"Black"}
   const appleInfo={type:"Fuji",color:"Red"}
   const showCarInfo= carInfo.brand!==undefined && carInfo.color!==undefined
+  const carList=[
+    {brand:"BMW",color:"Red"},
+    {brand:"Ford",color:"Green"},
+    {brand:"Tesla",color:"Black"},
+  ];
+
+  const numberList=[1,2,3,4,5,6,6,6];
   return (
     <>
       <h1>Who lives in my garage?</h1>
@@ -15,6 +22,12 @@ function Garage() {
       { showCarInfo && <Car carInfo={carInfo} />}
       <Apple appleInfo={appleInfo}/>
       {isDoorOpened ?<h2>Garage door is opened</h2>:<h2>Garage door is closed</h2>}
+        <ul>
+            {carList.map((carInfo)=><li key={carInfo.brand}><Car carInfo={carInfo}/></li>)} 
+        </ul>
+         <ul>
+            {numberList.map((e,index)=><p key={index}>{e}</p>)} 
+        </ul>
     </>
   );
 }
