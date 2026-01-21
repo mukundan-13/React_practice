0f74bbad0d7eab9215e209ec012ba812e0cc06bb
mukundan-13 <mukundanproff@gmail.com>
2026-01-21 11:33:59 +0530

    Conditional rendering  added

--- day1_learning/my-react-app/src/components/Apple.jsx
+++ day1_learning/my-react-app/src/components/Apple.jsx
@@ -1,6 +1,8 @@
-function Apple(){
-    return(
-        <h3>I am Apple</h3>
-    );
+function Apple(props) {
+  const { appleInfo } = props;
+  const text = `I am a ${appleInfo.color} ${appleInfo.type} Apple`;
+
+  return <h3>{text}</h3>;
 }
+
 export default Apple;
--- day1_learning/my-react-app/src/components/Garage.jsx
+++ day1_learning/my-react-app/src/components/Garage.jsx
@@ -1,13 +1,20 @@
 import Car from "./Car";
-
+import Apple from "./Apple";
 function Garage() {
-//   props added
-  const carInfo={brand:"Ferrari",color : "red"}
 
+  const isDoorOpened=false;
+  const carInfo={}
+  //const carInfo={brand:"Ferrari",color:"Black"}
+  const appleInfo={type:"Fuji",color:"Red"}
+  const showCarInfo= carInfo.brand!==undefined && carInfo.color!==undefined
   return (
     <>
       <h1>Who lives in my garage?</h1>
-      <Car carInfo={carInfo} />
+      {/* using ternary and using && operator */}
+      {/* { showCarInfo?<Car carInfo={carInfo} /> : null} */}
+      { showCarInfo && <Car carInfo={carInfo} />}
+      <Apple appleInfo={appleInfo}/>
+      {isDoorOpened ?<h2>Garage door is opened</h2>:<h2>Garage door is closed</h2>}
     </>
   );
 }
